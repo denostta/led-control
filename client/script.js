9db61a7f2b1839b441e379f7a9395d79d73cf8e2
Anthony Denosta <ansterd644@gmail.com>
2024-02-14 02:33:17 +0800

    replaced the server address

--- client/script.js
+++ client/script.js
@@ -2,11 +2,14 @@ const ledToggle = document.getElementById("ledToggle");
 const statusText = document.getElementById("status");
 
 ledToggle.addEventListener("change", async () => {
-  const response = await fetch("http://localhost:3000/api/led", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: `ledStatus=${ledToggle.checked}`,
-  });
+  const response = await fetch(
+    "https://led-control-vp8i.onrender.com/api/led",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: `ledStatus=${ledToggle.checked}`,
+    }
+  );
   if (response.ok) {
     statusText.textContent = `LED is ${ledToggle.checked ? "ON" : "OFF"}`;
     console.log("response ok");
